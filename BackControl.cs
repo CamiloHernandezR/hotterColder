689eee9a10c97e7994ee96050a2f1059305dc718
Camilo Hernandez <CamiloHernandezR@noreply.com>
2021-03-08 22:00:26 -0600

    demo uno

---
+++ BackControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hotterColder
+{
+    class BackControl
+    {
+        public int random { get; set; }
+        public int lastGuess { get; set; }
+
+        public BackControl(int random)
+        {
+            this.random = random;
+        }
+        public bool firstGuess()
+        {
+            Console.WriteLine("Please make a guess");
+            var input = Console.ReadLine();
+            //validations process
+            int guess = Convert.ToInt32(input);
+
+            if (guess <= (random + 10) && guess >= (random - 10))
+            {
+                Console.WriteLine("hot");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Cold");
+                return false;
+            }
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace hotterColder
     {
         static void Main(string[] args)
         {
-            
+            Random ran = new Random();
+            BackControl mistery = new BackControl(ran.Next(0, 101));
+            Console.WriteLine("random number has been chosen {0}",mistery.random);
+            while (!mistery.firstGuess());
+            Console.ReadKey();
+
+
         }
     }
 }
