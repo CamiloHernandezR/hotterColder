22ff22de6a72b6881ee6c42a553c360366275318
Camilo Hernandez <CamiloHernandezR@noreply.com>
2021-03-09 00:06:49 -0600

    validaciones

--- BackControl.cs
+++ BackControl.cs
@@ -13,23 +13,64 @@ namespace hotterColder
         {
             this.random = random;
         }
-        public bool firstGuess()
+        public int OnlyNumbers()
         {
-            Console.WriteLine("Please make a guess");
-            var input = Console.ReadLine();
-            //validations process
-            int guess = Convert.ToInt32(input);
 
-            if (guess <= (random + 10) && guess >= (random - 10))
+            while (true)
             {
-                Console.WriteLine("hot");
-                return true;
+                try
+                {
+                    string input = Console.ReadLine();
+
+                    int num = Convert.ToInt32(input);
+                    return num;
+                }
+                catch (Exception e)
+                {
+                   
+                    Console.WriteLine("Please type a valid integer number");
+
+                }
+            }
+        }
+        public bool FirstGuess(int guess)
+        {
+
+            if (guess == random) { return true; }
+
+            else if (guess <= (random + 10) && guess >= (random - 10))
+            {
+                Console.WriteLine("hot"); 
             }
             else
             {
-                Console.WriteLine("Cold");
-                return false;
+                Console.WriteLine("Cold");                
             }
+            lastGuess = guess;
+            return false;
+        }
+        public int Difference(int num)
+        {
+           int dif = Math.Abs( num - random);
+            return dif;
+        }
+        public bool Guess(int input)
+        {
+            if (input == random)
+            {
+                
+                return true;
+            }
+            else if (Difference(input)<Difference(lastGuess))
+            {
+                Console.WriteLine("hotter");
+            }
+            else
+            {
+                Console.WriteLine("Colder");
+            }
+            lastGuess = input;
+                return false;
         }
         
     }
--- Program.cs
+++ Program.cs
@@ -6,12 +6,17 @@ namespace hotterColder
     {
         static void Main(string[] args)
         {
-            Random ran = new Random();
+            Random ran = new Random();            
             BackControl mistery = new BackControl(ran.Next(0, 101));
-            Console.WriteLine("random number has been chosen {0}",mistery.random);
-            while (!mistery.firstGuess());
-            Console.ReadKey();
-
+            Console.WriteLine("random number has been chosen {0}", mistery.random);
+            Console.WriteLine("Make a guess");            
+            bool win = mistery.FirstGuess(mistery.OnlyNumbers());
+            while (!win)
+            {
+                Console.WriteLine("Please make a guess");               
+                win = mistery.Guess(mistery.OnlyNumbers());
+            }
+            Console.WriteLine("You got it!’");
 
         }
     }
